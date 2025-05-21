1bf368d1ee72274bf3babe10c4306426f09cd3e9
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-21 10:21:53 +0800

    05211021

---
+++ hi1/src/main/java/tw/brad/utils/HibernateUtil.java
@@ -0,0 +1,12 @@
+package tw.brad.utils;
+
+import org.hibernate.SessionFactory;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		
+		return null;
+	}
+}
